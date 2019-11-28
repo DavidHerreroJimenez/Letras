54489b094ec3e500a7caa8fe501712bea661c0ed
David Herrero Jiménez <daheji@gmail.com>
2019-11-28 22:46:28 +0100

    added new struct

---
+++ Letras/Model/WordResult.swift
@@ -0,0 +1,18 @@
+//
+//  WordResult.swift
+//  Letras
+//
+//  Created by David Herrero Jiménez on 28/11/2019.
+//  Copyright © 2019 -. All rights reserved.
+//
+
+import Foundation
+
+struct WordResult{
+    
+    let word: String
+    let score: Int
+    let isBonus: Bool
+    
+    
+}
